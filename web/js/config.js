22c14eed51432448f5d87f2bb752bf9225c68b03
LcsCamacho <lucas.camachofilhoo@gmail.com>
2026-07-07 19:31:05 -0300

    Refactor API_URL configuration for environment detection
    - Introduced a new `isLocal` variable to dynamically set the `API_URL` based on the hostname and protocol, allowing for easier development and testing on local environments.

--- web/js/config.js
+++ web/js/config.js
@@ -1,3 +1,9 @@
 const API_URL_DEV = "http://localhost:3000";
 const API_URL_PROD = "https://new-floor-sistema-erp.onrender.com";
-const API_URL = API_URL_PROD; // Change to API_URL_DEV for development
+
+const isLocal =
+  location.hostname === "localhost" ||
+  location.hostname === "127.0.0.1" ||
+  location.protocol === "file:";
+
+const API_URL = isLocal ? API_URL_DEV : API_URL_PROD;
